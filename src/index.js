83baa2c98f79396cad3888784ce5059562d847e4
M.Cepphus <m.chystisk@gmial.com>
2025-09-07 01:36:25 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (!matrix) {
+    return [];
+  }
+  let res = [];
+  let i = 0;
+  for (i; i < matrix.length; i += 1) {
+    if (i % 2 !== 0) {
+      matrix[i].reverse();
+    }
+    res = res.concat(matrix[i]);
+  }
+  return res;
 };
